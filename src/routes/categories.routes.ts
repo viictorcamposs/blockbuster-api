2e9a1e2863d74119008762722cc7f654deb83fba
Victor Campos <campos.viictor@gmail.com>
2021-06-09 11:24:34 -0300

    feat: create categoriesRoutes & connect to server

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,5 @@
+import { Router } from "express";
+
+const categoriesRoutes = Router();
+
+export { categoriesRoutes };
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,10 @@
 import express from "express";
 
+import { categoriesRoutes } from "./routes/categories.routes";
+
 const app = express();
+
+app.use(express.json());
+app.use("/categories", categoriesRoutes);
 
 app.listen(3030, () => console.log("Server is running on port 3030"));
