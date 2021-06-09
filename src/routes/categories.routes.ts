07e799dd2f5bdacce58830192952556bb6ea6ab5
Victor Campos <campos.viictor@gmail.com>
2021-06-09 12:16:58 -0300

    feat: add post http method

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,5 +1,16 @@
 import { Router } from "express";
 
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
+
 const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository();
+
+categoriesRoutes.post("/", (request, response) => {
+  const { name } = request.body;
+
+  const newCategory = categoriesRepository.create({ name });
+
+  return response.status(201).json(newCategory);
+});
 
 export { categoriesRoutes };
